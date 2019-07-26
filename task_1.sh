47f171098f6507868b1aab71474ca83cc29908a0
cheburashka37 <48180167+cheburashka37@users.noreply.github.com>
2019-07-26 14:45:44 +0300

    edit task_1.sh

--- task_1.sh
+++ task_1.sh
@@ -1,17 +1,1 @@
-#!/bin/bash
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements.  See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
-# 
-#    http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-exec $(dirname $0)/kafka-run-class.sh kafka.admin.TopicCommand "$@"
+/usr/hdp/current/kafka-broker/bin/kafka-topics.sh —zookeeper 10.156.0.3, 10.156.0.4, 10.156.0.5 —create —topic sschokorov —partitions 3 —replication-factor 3
